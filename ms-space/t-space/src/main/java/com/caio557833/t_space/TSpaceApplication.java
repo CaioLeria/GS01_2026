be2e5137176755745c73659f7fb3084f3bab171e
Bruno Itikawa <brunoitikawa@gmail.com>
2026-05-26 21:44:19 -0300

    adicionando projeto da cida

---
+++ ms-space/t-space/src/main/java/com/caio557833/t_space/TSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.caio557833.t_space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TSpaceApplication.class, args);
+	}
+
+}
---
+++ ms-space/t-space/src/test/java/com/caio557833/t_space/TSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio557833.t_space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
